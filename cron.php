e546a78ef13c28905267faef27fc46e5b0375168
giuem <453133693@qq.com>
2014-08-25 13:19:59 +0800

    修复部分小号无法使用的问题

--- cron.php
+++ cron.php
@@ -18,7 +18,7 @@ if(!isLogin($bduss)){
 $type=array('reply','at');
 $lastpid = array(getSQLPid($type[0]),getSQLPid($type[1])); 
 //判断数据库中是否记录PID
-if($lastpid[0] == '' or $lastpid[1] == ''){
+if($lastpid[0] == '' and $lastpid[1] == ''){
     $nowpid = array(getLastPid($bduss,$type[0]),getLastPid($bduss,$type[1]));
 	$sql="INSERT INTO pid(reply,at) values ($nowpid[0],$nowpid[1])";
 	$conn->query($sql);
