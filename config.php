896f542ab6bf2b9c55c27f4f13e6c4e208b634e9
giuem <453133693@qq.com>
2014-08-30 20:36:18 +0800

    更新大量api，一起hi起来吧

--- config.php
+++ config.php
@@ -2,8 +2,17 @@
 //设置BDUSS
 $bduss='xx';       //不需要带BDUSS= 
 //机器人的名字 如 TB小冰
-$name = 'TB小冰';
-
+$name = ' TB小冰';
+/*
+ * 关于api说明
+ * xiaoji：就是原来的接口，可以调教的
+ * tuling：图灵机器人的接口，主要是查询牛逼，需要填写key
+ * xiaoi3：第三方小i接口
+ * simsimi3：第三方小i接口，速度快一些
+ * simsimi：官方接口，速度慢，可能失效
+ * 想用什么就在下面写上
+ */
+$api='xiaoji';
 if(defined('SAE_MYSQL_DB')) {
 // ------------------ SAE数据库设定 ------------------
 	$db_server = SAE_MYSQL_HOST_M;						// 已自动设置好，无需干预
@@ -14,8 +23,8 @@ if(defined('SAE_MYSQL_DB')) {
 // -------------- END SAE数据库设定 ------------------
 }else {
 // ------------------ 非SAE数据库设定 ------------------
-	$db_server = 'localhost';			// 数据库服务器地址
-	$db_port = '3306';				// 数据库端口
+	$db_server = '';			// 数据库服务器地址
+	$db_port = '';				// 数据库端口
 	$db_username = '';			// 数据库用户名
 	$db_password = '';			// 数据库密码
 	$db_name = '';				// 数据库名
--- function/robot.php
+++ function/robot.php
@@ -4,25 +4,51 @@
  * 自定义机器人函数
  */
 //主函数
-function talk($content) {
+function talk($content,$api) {
 	global $name;
 	// 对内容进行处理
 	$content = str_ireplace("@$name",'',$content);
 	$content = str_ireplace("回复 $name :",'',$content);
-	// 有时候无法显示 回复的内容，就要有一个默认回复
+	// 如果只是单单@的话，会被过滤为空。api无法识别，所以要设置默认回复
 	if($content == ''){
-		return '你好';
+		return '您好，贴吧小冰很高兴为您服务';
+	}
+	switch ($api){
+		case 'xiaoji':
+			$re = xiaoji($content);
+			break;
+		case 'tuling':
+			$re = tuling($content);
+			break;
+		case 'xiaoi3':
+			$key = '';//填写key
+			$re = tulin($content,$key);
+			break;
+		case 'simsimi3':
+			$re = simsimi3($content);
+			break;
+		case 'simsimi':
+			$re = simsimi($content);
+			break;
+		default:
+			$re = xiaoji($content);
+			
 	}
 	
-	$re = xiaoji($content);
+	
+	
+	
 	// 有时候返回内容为空 就要有一个默认回复
 	if($re == ''){
-		return '你好';
+		return 'Hi!';
 	}else {
 		return $re;
 	}
 }
-//和dun的小红鸡一样，可调教
+
+/*
+ * 和dun的小红鸡一样的接口，可调教
+ */
 function xiaoji($content){
 	//正则匹配 调教
 	if(preg_match('/\[问：(.*?) 答：(.*?)\]/',$content,$teach) == 1){
@@ -42,12 +68,93 @@ function teach($q,$a){
 		return '调教失败！';
 	}
 }
+/*
+ * 图灵机器人官网地址： http://www.tuling123.com/
+ */
+function tuling($content,$key){
+	$re = json_decode(HTTPclient('http://www.tuling123.com/openapi/api?key='.$key.'&info='.$content),true);
+	$code = $re['code'];
+	switch ($code){
+		//文本类数据
+		case 100000:
+			$content = $re['text'];
+			break;
+		//网址类数据
+		case 200000:
+			$content = $re['text'].$re['url'];
+			break;
+		//新闻
+		case 302000:
+			$list = $re['list'];
+			$i = rand(0,count($list)-1);
+			$list = $list[$i];
+			$content = $re['text'].'：'.$list['article'];
+			break;
+		//火车
+		case 305000:
+			$list = $re['list'];
+			$i = rand(0,count($list)-1);
+			$list = $list[$i];
+			$content = '起始站：'.$list['start'].'，到达站：'.$list['terminal'].'，开车时间：'.$list['starttime'].'，到达时间：'.$list['endtime'].'。亲，更多信息请上网查询哦！';
+			break;
+		//飞机
+		case 306000:
+			$list = $re['list'];
+			$i = rand(0,count($list)-1);
+			$list = $list[$i];
+			$content = '航班：'.$list['flight'].'，航班路线：'.$list['route'].'，起飞时间：'.$list['starttime'].'，到达时间：'.$list['endtime'].'。亲，更多信息请上网查询哦！';
+			break;
+		//彩票
+		case 310000:
+			$list = $re['list'];
+			$i = rand(0,count($list)-1);
+			$list = $list[$i];
+			$content = $list['info'].'中奖号码：'.$list['number'].'。亲，小赌怡情，大赌伤身哦！';
+			break;
+		//次数用完或者不能用了
+		case 40004:
+			$content = xiaoji($content);
+			break;
+		default:
+			$content = xiaoji($content);;
+	}
+	return $content;
+}
 
 
-//第三方小i机器人接口，不怎么好，可以查天气什么的
+
+
+/*
+ * 第三方小i机器人接口，不怎么好，可以查天气什么的
+ */
 function xiaoi3($content){
     $re=json_decode(file_get_contents("http://rmbz.net/Api/AiTalk.aspx?key=rmbznet&talk=xiaoi&word=".$content),true);
     $re = $re['content'];
     return $re;
+}
+/*
+ * 第三方小黄鸡接口
+ */
+function simsimi3($content){
+	$re = HTTPClient('http://api.mrtimo.com/Simsimi.ashx?parm='.$content);
+	return $re;
+}
+/*
+ * 盗用官方的api
+ */
+function simsimi($content){
+	$ch = curl_init();
+	curl_setopt($ch,CURLOPT_URL,'http://www.simsimi.com/talk.htm');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HEADER,1);
+	curl_setopt($ch, CURLOPT_NOBODY, false);
+	$rs = curl_exec($ch);
+	preg_match_all('/Set-Cookie: (.+)=(.+)$/m', $rs, $regs);
+	foreach($regs[1] as $i=>$k);
+	$cc=str_replace(' Path','' ,$k);
+	$cc='simsimi_uid=507454034223;'.$cc;
+	$re = HTTPClient('http://www.simsimi.com/func/reqN?lc=ch&ft=1.0&req='.$content.'&fl=http%3A%2F%2Fwww.simsimi.com%2Ftalk.htm',$cc);
+	$re = json_decode($re,true);
+	return $re['sentence_resp'];
 }
 ?>
