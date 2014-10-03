91f0ddb44793ae366e14dea446a743b16ec60ecc
giuem <453133693@qq.com>
2014-10-03 21:08:19 +0800

    Add logout

--- lib/ui.php
+++ lib/ui.php
@@ -2,6 +2,10 @@
 if (!defined('IS_GIUEM')) exit();
 class ui{
 	public static function show(){
+		if($_GET['action']=='logout'){
+			logout();
+			header('Location: login.php');
+		}
 		include SYSTEM_ROOT.'/templates/header.php';
 		$file=SYSTEM_ROOT.'/templates/'.$_GET['action'].'.php';
 		if(file_exists($file))
