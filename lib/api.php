e6a668a8548c4334bc3a75868f585b17912b705e
giuem <i@giuem.com>
2015-08-28 21:09:54 +0800

    Fix simsimi api

--- lib/api.php
+++ lib/api.php
@@ -90,18 +90,18 @@ function simsimi3($content){
 	return $re;
 }
 function simsimi($content){
-	$ch = curl_init();
-	curl_setopt($ch,CURLOPT_URL,'http://www.simsimi.com/talk.htm');
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_HEADER,1);
-	curl_setopt($ch, CURLOPT_NOBODY, false);
-	$rs = curl_exec($ch);
-	preg_match_all('/Set-Cookie: (.+)=(.+)$/m', $rs, $regs);
-	foreach($regs[1] as $i=>$k);
-	$cc=str_replace(' Path','' ,$k);
-	$cc='simsimi_uid=507454034223;'.$cc;
-	$re = fetch('http://www.simsimi.com/func/reqN?lc=ch&ft=1.0&req='.$content.'&fl=http%3A%2F%2Fwww.simsimi.com%2Ftalk.htm',$cc);
-	$re = json_decode($re,true);
-	return $re['sentence_resp'];
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,'http://www.simsimi.com/talk.htm');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HEADER,1);
+    curl_setopt($ch, CURLOPT_NOBODY, false);
+    $rs = curl_exec($ch);
+    preg_match_all('/Set-Cookie: (.+)=(.+)$/m', $rs, $regs);
+    foreach($regs[1] as $i=>$k);
+    $cc=str_replace(' Path','' ,$k);
+    $cc='simsimi_uid='.rand().';'.$cc;
+    $re = url_get('http://www.simsimi.com/func/reqN?lc=ch&ft=0.0&req='.urlencode($content).'&fl=http%3A%2F%2Fwww.simsimi.com%2Ftalk.htm&reqType=','',$cc);
+    $re = json_decode($re,true);
+    return $re['sentence_resp'];
 }
 /*PHP END*/
